f4bf21efa341593053333f05f8f89e985b6b5d73
wlchn <wlchn@qq.com>
2017-07-05 20:21:02 +0800

    can selected post and pass json

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "bytes"
     "net/http"
     "io/ioutil"
     "github.com/andlabs/ui"
@@ -9,46 +10,72 @@ import (
 
 func main() {
     err := ui.Main(func() {
-        name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
         box := ui.NewVerticalBox()
 
         urlEntry := ui.NewEntry()
-        getButton := ui.NewButton("Get")
+        jsonEntry := ui.NewEntry()
+        goButton := ui.NewButton("Go->")
         resLabel := ui.NewLabel("")
-
-        box.Append(ui.NewLabel("Enter your name:"), false)
-        box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
+        selectCombobox := ui.NewCombobox()
 
         box.Append(ui.NewLabel("Input url:"), false)
         box.Append(urlEntry, false)
-        box.Append(getButton, false)
+        box.Append(ui.NewLabel("Input json:"), false)
+        box.Append(jsonEntry, false)
+        box.Append(selectCombobox, false)
+        box.Append(goButton, false)
         box.Append(resLabel, false)
+
+        selectCombobox.Append("Get")
+        selectCombobox.Append("Post")
+
+        selectCombobox.SetSelected(0)
 
         window := ui.NewWindow("Hello", 400, 300, false)
         window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            fmt.Printf("button pressed\n")
-            greeting.SetText("Hello, " + name.Text() + "!")
-        })
-        getButton.OnClicked(func(*ui.Button) {
-            fmt.Printf("getButton pressed\n")
-            resp, err := http.Get(urlEntry.Text())
-            if err != nil {
-                resLabel.SetText(err.Error())
-                return
-            }
-            if resp.StatusCode == 200 {
-                bodyBytes, err := ioutil.ReadAll(resp.Body)
-                if err != nil {
-                    return
-                }
-                bodyString := string(bodyBytes)
-                fmt.Printf(bodyString)
-                resLabel.SetText(bodyString)
+
+        goButton.OnClicked(func(*ui.Button) {
+            fmt.Printf("goButton pressed\n")
+            selectedIndex := selectCombobox.Selected()
+            switch selectedIndex {
+                case 0:
+                    resp, err := http.Get(urlEntry.Text())
+                    if err != nil {
+                        resLabel.SetText(err.Error())
+                        return
+                    }
+                    if resp.StatusCode == 200 {
+                        bodyBytes, err := ioutil.ReadAll(resp.Body)
+                        if err != nil {
+                            return
+                        }
+                        bodyString := string(bodyBytes)
+                        fmt.Printf(bodyString)
+                        resLabel.SetText(bodyString)
+                    }
+                case 1:
+                    url := urlEntry.Text()
+                    fmt.Println("URL:>", url)
+                    jsonStr := jsonEntry.Text()
+
+                    var jsonBytes = []byte(jsonStr)
+                    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
+                    req.Header.Set("Content-Type", "application/json")
+
+                    client := &http.Client{}
+                    resp, err := client.Do(req)
+                    if err != nil {
+                        panic(err)
+                    }
+                    defer resp.Body.Close()
+
+                    fmt.Println("response Status:", resp.Status)
+                    fmt.Println("response Headers:", resp.Header)
+                    body, _ := ioutil.ReadAll(resp.Body)
+                    resLabel.SetText(string(body))
+                    fmt.Println("response Body:", string(body))
+                default:
+                    fmt.Printf("no selected")
             }
 
         })
